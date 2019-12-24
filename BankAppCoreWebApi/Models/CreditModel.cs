c0963ecb6649ff88fa48c80614a98039cf5ef38f
Muhammet Akvardar <34101771+sreilus@users.noreply.github.com>
2019-12-25 01:58:14 +0300

    Create CreditModel

---
+++ BankAppCoreWebApi/Models/CreditModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAppCoreWebApi.Models
+{
+	public class CreditModel
+	{
+		public int krediMiktari { get; set; }
+		public int yas { get; set; }
+		public int evDurumu { get; set; }
+		public int aldigi_kredi_sayi { get; set; }
+		public int telefonDurumu { get; set; }
+	}
+}
